b3c67653d442e84b14d0cecdb4012fd0a6a69174
dual25 <dual_2536@126.com>
2018-01-17 15:25:22 +0800

    pthread_join pthread_detach

--- main.c
+++ main.c
@@ -6,11 +6,31 @@
 
 void* PrintHello(void *threadid)
 {
+   pthread_detach(pthread_self());
    std::cout << *(int*)(threadid) <<"wdqwd" << std::endl;
+//   sleep(1000);
+
    pthread_exit(NULL);
 }
 
+// pthread_detach() 
 
+int main(int arge, char *argv[])
+{
+    pthread_t id;
+    int nParam = 5;
+    int rc = pthread_create(&id, NULL, PrintHello, (void*)&nParam);
+ 
+    void* status;
+    pthread_join(id, &status);
+    //pthread_detach(id);
+
+    std::cout << "main line" << std::endl;
+
+    return 0;
+}
+
+/*
 int main(int argc, char *argv[])
 {
     std::cout << "begin" << std::endl;
@@ -20,8 +40,11 @@ int main(int argc, char *argv[])
     int rc = pthread_create(&t, NULL, PrintHello, (void*)&param);
     std::cout << rc << std::endl;
 
-    sleep(1000);    
+    void * status;
+    pthread_join(t, &status); 
     return 0;
 }
+
+*/
 
 
