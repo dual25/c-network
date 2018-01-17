b27dbb965ba60039597444c73029bec1d0afdebc
dual25 <dual_2536@126.com>
2018-01-17 21:10:57 +0800

    cond

--- main.c
+++ main.c
@@ -31,40 +31,94 @@ int main(int arge, char *argv[])
 */
 
 // mutex
-
-
+/*
 pthread_mutex_t mutexnum;
-
 void* CalcNum(void* param)
 {
    std::cout << "in" << std::endl;
    pthread_t t = pthread_self();
+   for(int i = 0; i < 10; ++i)
+   {
    pthread_mutex_lock(&mutexnum);
    (*(int*)param) += 5;
    std::cout << (int)t << ":" << *(int*)param << std::endl;
    pthread_mutex_unlock(&mutexnum);
+   sleep(1);
+   }
 }
 
 int main(int argc, char *argv[])
 {
     int nNumber = 0;
-    pthread_t t[2];
-
-    
+    pthread_t t[3];
+ 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    for(int i = 0; i < 2; ++i)
+    for(int i = 0; i < 3; ++i)
     {
         pthread_create(&t[i], &attr, CalcNum, &nNumber);
     }
 
-    for(int i = 0; i < 2; ++i)
+    for(int i = 0; i < 3; ++i)
     {
         void* status;
         pthread_join(t[i], &status);
     }
 
     return 0;
+}
+*/
+
+// cond
+    pthread_cond_t cont;
+    pthread_mutex_t mutex;
+
+void* waitcond(void* param)
+{
+    for(int i = 0; i < 10; ++i)
+    {
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cont, &mutex);
+    (*(int*)param) += 5;
+    std::cout << "wait" << ":" << *(int*)param << std::endl;
+    pthread_mutex_unlock(&mutex);
+    }
+
+}
+
+void* sendcond(void* param)
+{
+        for(int i = 0; i < 10; ++i)
+    {
+    pthread_mutex_lock(&mutex);
+    (*(int*)param) += 5;
+    std::cout << "send" << ":" << *(int*)param << std::endl;
+    pthread_mutex_unlock(&mutex);
+    int n = pthread_cond_signal(&cont);
+    std::cout << n << std::endl;
+
+    sleep(1);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+
+    pthread_t id[2];
+
+    pthread_attr_t attr;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cont, NULL);
+
+    pthread_attr_init(&attr);
+    int nParam = 0;
+    pthread_create(&id[0], &attr, waitcond, &nParam);
+    pthread_create(&id[1], &attr, sendcond, &nParam);
+
+    void* status;
+    pthread_join(id[0], &status);
+    pthread_join(id[1], &status);
+
 }
 
 
